625312c6dd8542bd0b738ff74b831e7cd8126af3
NAZIM AMEER <nazimameerpp@gmail.com>
2023-11-04 13:56:34 +0530

    setup main.ts

---
+++ gateway/main.ts
@@ -0,0 +1,20 @@
+import express, { json, urlencoded } from 'express';
+import proxy from 'express-http-proxy';
+
+
+const startServer = () => {
+    try {
+        const app = express();
+        app.use(json());
+        app.use(urlencoded);
+        const port = process.env.PORT || 8000;
+        
+        // use Proxy
+
+        app.use('/user', proxy("http://localhost:8002"))
+
+
+    } catch (error) {
+        
+    }
+}
