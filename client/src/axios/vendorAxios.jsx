5f54a3e2ea733cde60a44d733ca2e0bcab22d283
NAZIM AMEER <nazimameerpp@gmail.com>
2023-11-06 23:07:36 +0530

    integrate with server

---
+++ client/src/axios/vendorAxios.jsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+const baseUrl = 'http://localhost:8000'
+const EmployeeInstance = axios.create({
+  baseURL: baseUrl,
+});
+
+EmployeeInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("vendorToken"); // taking token from local storage
+
+    if (token) { 
+      // checking if token is present or not
+
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default EmployeeInstance;
