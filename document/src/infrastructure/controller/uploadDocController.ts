f2a24547f560b939ff8b1a5ffe6c9f387834ab90
NAZIM AMEER <nazimameerpp@gmail.com>
2023-11-29 00:58:08 +0530

    changes

--- document/src/infrastructure/controller/uploadDocController.ts
+++ document/src/infrastructure/controller/uploadDocController.ts
@@ -2,6 +2,6 @@ import {Request, Response} from 'express';
 
 export const uplodDoc = (req: Request, res:Response) => {
     const fileData = req.file;
-    console.log(req.body);
-    res.write("hei")
+    const { vendor, shippingDate, quantity, productName } = req.body;
+    console.log(fileData);
 }
