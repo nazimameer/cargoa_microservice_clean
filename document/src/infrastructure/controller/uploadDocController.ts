a811b317863f2d22a5e5c953c280491e7d8fc18b
NAZIM AMEER <nazimameerpp@gmail.com>
2023-11-28 21:54:47 +0530

    add multer for file

--- document/src/infrastructure/controller/uploadDocController.ts
+++ document/src/infrastructure/controller/uploadDocController.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from 'express';
 
 export const uplodDoc = (req: Request, res:Response) => {
+    const fileData = req.file;
     console.log(req.body);
     res.write("hei")
 }
--- document/src/infrastructure/routes/doc-routes.ts
+++ document/src/infrastructure/routes/doc-routes.ts
@@ -1,7 +1,10 @@
 import express from "express";
 const router = express.Router();
+import multer from 'multer';
+const storage = multer.memoryStorage(); // Use memory storage for simplicity
+const upload = multer({ storage: storage });
 import { uplodDoc } from '../controller';
-router.post('/upload', uplodDoc);
+router.post('/upload',upload.single('doc'), uplodDoc);
 
 
 export default router;
